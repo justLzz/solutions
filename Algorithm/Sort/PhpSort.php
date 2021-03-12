d7d39982525fb9d10fdba2fdb4842fe524870894
justLzz <1072084962@qq.com>
2021-03-12 13:30:14 +0800

    选择排序

--- Algorithm/Sort/PhpSort.php
+++ Algorithm/Sort/PhpSort.php
@@ -15,13 +15,36 @@ class PhpSort
             }
         }
         return $arr;
-
     }
+
+    //选择排序
+    public function selectSort($arr)
+    {
+        //每一趟选择出最小或最大，
+        for ($i=0;$i<count($arr);$i++)
+        {
+            $k=$i;//数组下标
+            for ($j=$i+1;$j<count($arr);$j++)
+            {
+                //选最小,从小到大排序
+                if ($arr[$j] < $arr[$k])
+                {
+                    $k=$j;
+                }
+            }
+            //第i趟位置的元素和最小元素交换位置
+            list($arr[$i], $arr[$k]) = [$arr[$k], $arr[$i]];
+        }
+        return $arr;
+    }
+
+    //
+
 
 }
 
 $arr = [12,8,3,7,2,83,75,6,78,34,89,10,11];
 $sort = new PhpSort();
-var_dump($sort->bubbleSort($arr));
+var_dump($sort->selectSort($arr));
 
 
