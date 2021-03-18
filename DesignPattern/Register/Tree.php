9495e08d3a5e3103b893516202ee3e7e30b28d44
justLzz <1072084962@qq.com>
2021-03-18 17:14:40 +0800

    注册模式

---
+++ DesignPattern/Register/Tree.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Solutions\DesignPattern\Register;
+
+
+class Tree
+{
+    protected static $tree = [];
+
+    public static function set($k, $v)
+    {
+        self::$tree[$k] = $v;
+    }
+
+    public static function get($k)
+    {
+        return self::$tree[$k];
+    }
+}
