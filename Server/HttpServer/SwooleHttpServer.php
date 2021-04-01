8fb08827ae840f9a86fd86185253e5d42a944846
justLzz <1072084962@qq.com>
2021-04-01 09:26:13 +0800

    httpServer修改

--- Server/HttpServer/SwooleHttpServer.php
+++ Server/HttpServer/SwooleHttpServer.php
@@ -3,8 +3,31 @@
 
 namespace Solutions\Server\HttpServer;
 
+use Swoole\Http\Server;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
 
-class SwooleHttpServer
+/**
+ * swoole创建http服务
+ * Class SwooleHttpServer
+ * @package Solutions\Server\HttpServer
+ */
+abstract class SwooleHttpServer
 {
+    public function __construct()
+    {
+        $http = new Server('127.0.0.1', 9501);
+        $http->on('Request', function (Request $request, Response $response) {
+            $data = $request->getContent();
+            if ($this->dealData($data))
+            {
+                $response->end('ok');
+            }
+            $response->end('error');
 
+        });
+        $http->start();
+    }
+
+    abstract public function dealData($data):bool ;
 }
