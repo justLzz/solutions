20725cf590fa5572ac04b7cc11a918a938f4f858
justLzz <1072084962@qq.com>
2021-04-13 11:35:10 +0800

    结构

---
+++ src/Language/Php/Base/FilterFunction/Dfa/SensitiveWords.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Justlzz\Solutions\Language\Php\Base\FilterFunction\Dfa;
+
+/**
+ * 敏感词过滤
+ * Class SensitiveWords
+ * @package Justlzz\Solutions\Language\Php\Base\FilterFunction\Dfa
+ */
+class SensitiveWords
+{
+
+}
