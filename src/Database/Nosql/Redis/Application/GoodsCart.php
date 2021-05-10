cb1161098967435689a4c7ae5936c8bb96d78f0e
justLzz <1072084962@qq.com>
2021-05-10 16:25:41 +0800

    redis hash 实现购物车

---
+++ src/Database/Nosql/Redis/Application/GoodsCart.php
@@ -0,0 +1,176 @@
+<?php
+
+
+namespace Justlzz\Solutions\Database\Nosql\Redis\Application;
+
+/**
+ * 利用redis实现购物车
+ * Class GoodsCart
+ * @package Justlzz\Solutions\Database\Nosql\Redis\Application
+ */
+class GoodsCart
+{
+    protected $redis;
+    /**
+     *  购物车前缀
+     *
+     * @var string
+     */
+    protected $goodsCartPre = 'goods_cart_';
+
+    /**
+     * 用户id
+     *
+     * @var
+     */
+    protected $userId;
+
+    /**
+     * 商品id
+     * @var
+     */
+    protected $goodsId;
+
+    /**
+     * 商品相关信息
+     * @var
+     */
+    protected $goodsInfo;
+
+    /**
+     * 商品数量
+     * @var
+     */
+    protected $goodsNum;
+
+    public function __construct(\Redis $redis)
+    {
+        $this->redis = $redis;
+    }
+
+    public function setUserId($userId)
+    {
+        return $this->userId = $userId;
+    }
+
+    public function setGoodsNum($num)
+    {
+        return $this->goodsNum = $num;
+    }
+
+
+    public function setGoodsInfo($goodsInfo)
+    {
+        return $this->goodsInfo = $goodsInfo;
+    }
+
+    public function setGoodsId($goodsId)
+    {
+        return $this->goodsId = $goodsId;
+    }
+
+    public function setGoodsCartPre($pre)
+    {
+        return $this->goodsCartPre = $pre;
+    }
+
+    /**
+     * Notes:向购物车插入或更新一个商品数据
+     * @return bool|int
+     * @throws \Exception
+     */
+    public function insertCart()
+    {
+        if (!$this->userId) throw new \Exception('please set userId');
+        if (!$this->goodsId) throw new \Exception('please set goodsId');
+        if (!$this->goodsNum) throw new \Exception('please set goodsNum');
+        if (!$this->goodsInfo) throw new \Exception('please set goodsInfo');
+        $goodsValue = ['update_time' => time(),
+                        'num' => $this->goodsNum,
+                        'info' => $this->goodsInfo,
+                        'create_time' => time()];
+        return $this->redis->hSet($this->getKey(), $this->goodsId, json_encode($goodsValue));
+    }
+
+    /**
+     * Notes:更新购物车商品数据
+     * @return bool|int
+     * @throws \Exception
+     */
+    public function updateCart()
+    {
+        if (!$this->userId) throw new \Exception('please set userId');
+        if (!$this->goodsId) throw new \Exception('please set goodsId');
+        //商品原始信息
+        $goodsValue = $this->getGoodsValue();
+        if (isset($this->goodsInfo)) $goodsValue['info'] = $this->goodsInfo;
+        if (isset($this->goodsNum)) $goodsValue['num'] = $this->goodsNum;
+        $goodsValue['update_time'] = time();
+        return $this->redis->hSet($this->getKey(), $this->goodsId, json_encode($goodsValue));
+    }
+
+    /**
+     * Notes:获取一个购物车商品数据
+     * @return mixed
+     */
+    public function getGoodsValue()
+    {
+        return json_decode($this->redis->hGet($this->getKey(), $this->goodsId), true);
+    }
+
+    /**
+     * Notes:获取购物车key
+     * @return string
+     */
+    protected function getKey()
+    {
+        return $this->goodsCartPre . $this->userId;
+    }
+
+    /**
+     * Notes:获取购物车商品列表
+     * @throws \Exception
+     */
+    public function getGoodsList()
+    {
+        if (!$this->userId) throw new \Exception('please set userId');
+        $goodsList = $this->decodeArrayJson($this->redis->hGetAll($this->getKey()));
+        return $this->sortGoodsList($goodsList);
+    }
+
+    /**
+     * Notes:对商品按插入时间进行排序
+     * @param $goodsList
+     * @return mixed
+     */
+    public function sortGoodsList($goodsList)
+    {
+        array_multisort(array_column($goodsList, 'create_time'), SORT_DESC, $goodsList);
+        return $goodsList;
+    }
+
+    /**
+     * Notes:将数组中的json转化成数组
+     * @param $array
+     * @return mixed
+     */
+    public function decodeArrayJson($array)
+    {
+        foreach ($array as $key=>$value)
+        {
+            $array[$key] = json_decode($value,true);
+        }
+        return $array;
+    }
+
+    /**
+     * Notes:清空购物车
+     * @return int
+     */
+    public function emptyCart()
+    {
+        return $this->redis->del($this->getKey());
+    }
+
+
+}
---
+++ src/Example/Language/Php/App/OnlineRetailers/Api/goods_cart.php
@@ -0,0 +1,82 @@
+<?php
+require '/html/www/Solutions/vendor/autoload.php';
+
+use Justlzz\Solutions\Config\Config;
+use Justlzz\Solutions\Database\Nosql\Redis\Redis;
+use Justlzz\Solutions\Database\Nosql\Redis\Application\GoodsCart;
+
+/**
+ *购物车相关利用redis实现购物车
+ *使用hash结构(key field value) => (userId sku_id skuValue)
+ */
+
+//伪数据
+$uid = 1;
+$goodsId = 3;
+$goodsNum = 3;
+$goodsInfo =  ['goods_name' => '海蓝之家男装T恤',
+                'goods_pic' => 'http://fffsdf.awea.com/fef.jpg',
+                'goods_price' => 50];
+
+$config = new Config('redis');
+$redis = Redis::getInstance($config);
+$cart = new GoodsCart($redis);
+
+//插入一条购物车数据
+function insertCart($cart, $uid, $goodsId, $goodsNum, $goodsInfo)
+{
+    $cart->setUserId($uid);
+    $cart->setGoodsId($goodsId);
+    $cart->setGoodsNum($goodsNum);
+    $cart->setGoodsInfo($goodsInfo);
+    try {
+        return $cart->insertCart();
+    } catch (Exception $exception)
+    {
+        echo $exception->getMessage();
+    }
+}
+
+insertCart($cart, $uid, $goodsId, $goodsNum, $goodsInfo);
+
+//更新购物车商品信息
+function updateCart($cart, $uid, $goodsId, $goodsNum = null, $goodsInfo = null)
+{
+    $cart->setUserId($uid);
+    $cart->setGoodsId($goodsId);
+    $cart->setGoodsNum($goodsNum);
+    $cart->setGoodsInfo($goodsInfo);
+    try {
+        return $cart->updateCart();
+    } catch (Exception $exception)
+    {
+        echo $exception->getMessage();
+    }
+}
+updateCart($cart, $uid, $goodsId, $goodsNum + 1);
+
+//获取购物车列表
+function getGoodsList($cart, $uid)
+{
+    $cart->setUserId($uid);
+    try {
+        return $cart->getGoodsList();
+    } catch (Exception $exception)
+    {
+        echo $exception->getMessage();
+    }
+}
+var_dump(getGoodsList($cart, $uid));
+
+//清空购物车
+function emptyCart($cart, $uid)
+{
+    $cart->setUserId($uid);
+    try {
+        return $cart->emptyCart();
+    } catch (Exception $exception)
+    {
+        echo $exception->getMessage();
+    }
+}
+emptyCart($cart, $uid);
