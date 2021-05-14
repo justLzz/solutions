7fefaec6bb714a9c3438ebc7ff0f450e6139bbce
justLzz <1072084962@qq.com>
2021-05-14 09:35:30 +0800

    rabbitmq publisher confirm 机制 待完善

---
+++ src/Amqp/Rabbitmq/AsyncConfirm/Async.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Justlzz\Solutions\Amqp\Rabbitmq\AsyncConfirm;
+
+/**
+ * 基于swoole实现生产者异步接收回调通知
+ * Class Confirm
+ * @package Justlzz\Solutions\Amqp\Rabbitmq\AsyncConfirm
+ */
+class Async
+{
+
+}
