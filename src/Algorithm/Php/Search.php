fdc73ee1e71bd3e5995df524a9879cff6e1097d0
李执政 <zhizhengl@twcgroup.com>
2021-10-22 17:30:04 +0800

    查找算法

--- src/Algorithm/Php/Search.php
+++ src/Algorithm/Php/Search.php
@@ -75,5 +75,33 @@ class Search
         return current($arr);
     }
 
-}
+    /**
+     * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。
+     * 如果有多对数字的和等于s，则输出任意一对即可。
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[2,7] 或者 [7,2]
+     * @param $arr
+     * @param $sum
+     * @return array
+     */
+    public function getCombinationWithSumFromOneSortArray($arr, $sum)
+    {
+        //定义两个指针
+        $left = 0; //头指针
+        $right = count($arr) - 1; //尾指针
 
+        while ($right > $left)
+        {
+            if (($arr[$left] + $arr[$right]) > $sum)
+            {
+                $right--;
+            } else if (($arr[$left] + $arr[$right]) < $sum)
+            {
+                $left++;
+            }else{
+                return [$left, $right];
+            }
+        }
+    }
+
+}
